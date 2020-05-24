aa3683ed0cb7da674aba8a48991c793b418e3b44
jinn <booboo080yamaya@gmail.com>
2020-05-24 16:17:46 +0900

    Initial Commit

---
+++ ReportReserch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReportReserch
+//
+//  Created by 神野成紀 on 2020/05/24.
+//  Copyright © 2020 神野成紀. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
